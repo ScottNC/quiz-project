a58a70e98eb76004930c574ab74ea9d5108ffb53
ScottNC <133363623+ScottNC@users.noreply.github.com>
2023-08-04 21:15:35 +0100

    fixes function to decrypt apikey

--- server/decryptor.ts
+++ server/decryptor.ts
@@ -3,10 +3,10 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const iv = Buffer.alloc(16);
-
-export function decrypt(encrypted: string, algorithm: string, key: Buffer) {
-  const decryptor = crypto.createDecipheriv(algorithm, key, iv);
+export function decrypt(encrypted: string) {
+  const iv = Buffer.alloc(16);
+  const paddedKey = Buffer.concat([Buffer.from(process.env.ENCRYPTION_KEY!), Buffer.alloc(12)]);
+  const decryptor = crypto.createDecipheriv('aes-128-cbc', paddedKey, iv);
   let text = decryptor.update(encrypted, 'base64', 'utf8');
   text += decryptor.final('utf8');
   return text;
