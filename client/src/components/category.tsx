bff48574033203a9f1f05a600c44ab31bd320829
ScottNC <133363623+ScottNC@users.noreply.github.com>
2023-08-05 18:05:30 +0100

    calls the category api and displays on page

--- client/src/components/category.tsx
+++ client/src/components/category.tsx
@@ -1,8 +1,37 @@
-const Category = () => {
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { BASE_URL } from "../helpers/base_url";
+
+interface Category {
+  id: number;
+  name: string;
+}
+
+const Category: React.FC = () => {
+  const [categories, setCategories] = useState<Category[]>([]);
+
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+
+  const fetchCategories = async () => {
+    try {
+      const response = await axios.get(BASE_URL + '/category');
+      setCategories(response.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
   return (
-    <section>
-      <h1> Welcome to the Category page</h1>
-    </section>
+    <div>
+      <h1>Categories</h1>
+      <ul>
+        {categories.map((category) => (
+          <li key={category.id}>{category.name}</li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
