797a701d9a31888789fbc88aafb8447726e5df61
ScottNC <133363623+ScottNC@users.noreply.github.com>
2023-08-05 10:17:55 +0100

    includes external source type

--- server/utils/database_populator.ts
+++ server/utils/database_populator.ts
@@ -1,5 +1,11 @@
 import { queryDatabase } from "../db";
 
+interface ExternalSource {
+  name: string,
+  domain: string,
+  apikey: string
+}
+
 export async function populateDatabase() {
 
   await queryDatabase('DELETE FROM subcategory_relation');
@@ -7,10 +13,11 @@ export async function populateDatabase() {
   await queryDatabase('DELETE FROM question');
   await queryDatabase('DELETE FROM answer');
   await queryDatabase('DELETE FROM round');
+  await queryDatabase('DELETE FROM quiz');
 
-  const externalSources = await queryDatabase('SELECT name, domain, apikey FROM external_source');
+  const externalSources: ExternalSource[] = await queryDatabase('SELECT name, domain, apikey FROM external_source');
 
-  externalSources.forEach(async source => {
+  externalSources.forEach(async (source: ExternalSource) => {
     const { populate } = require('./external_sources/' + source.name);
     await populate(source.domain, source.apikey);
   });
