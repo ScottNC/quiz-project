7fbcc0646ceb6760226fc5a7a13ebe9ec5540332
kalyani110220 <kalyani.nelluri54@gmail.com>
2023-08-06 20:16:01 +0100

    chages in start.tsx of post data to fetch quizid

--- client/src/components/start.tsx
+++ client/src/components/start.tsx
@@ -4,26 +4,25 @@ import { BASE_URL } from "../helpers/base_url";
 
 interface Start {
   id: number;
-  name: string;
+  message: string;
 }
 
 const Start: React.FC = () => {
   const [starts, setStarts] = useState<Start[]>([]);
   const effectCalled = useRef<boolean>(false);
 
-  const quiz_id = 77; // hardcoded should be passed from previous page
+  // Replace the hardcoded quiz_id with the actual value you want to pass from the previous page
+  const quiz_id = 77; // Replace this with the actual quiz_id value
 
   useEffect(() => {
     if (effectCalled.current) return;
-    postStarts();
+    postStarts(); // Pass the quiz_id to the postStarts function
     effectCalled.current = true;
   }, []);
 
   const postStarts = async () => {
     try {
-      const response = await axios.post(BASE_URL + "/start", {
-        params: { quizId: quiz_id },
-      });
+      const response = await axios.post<Start[]>(`${BASE_URL}/start?quizId=${quiz_id}`);
       setStarts(response.data);
     } catch (error) {
       console.error("Error posting data:", error);
@@ -35,7 +34,7 @@ const Start: React.FC = () => {
       <h1> Welcome to the Start page</h1>
       <ul>
         {starts.map((start) => (
-          <li key={start.id}>{start.name}</li>
+          <li key={start.id}>{start.message}</li>
         ))}
       </ul>
     </section>
