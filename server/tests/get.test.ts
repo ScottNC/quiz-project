85ae150a238759ec6b92723c1a84b42fb4f539d5
ScottNC <133363623+ScottNC@users.noreply.github.com>
2023-08-09 17:54:17 +0100

    adds test for get /quiz

--- server/tests/get.test.ts
+++ server/tests/get.test.ts
@@ -1,4 +1,4 @@
-import { getCategory, getSubcategory } from '../services/get_information';
+import { getCategory, getQuiz, getSubcategory } from '../services/get_information';
 import * as db from '../db';
 
 jest.mock('../db');
@@ -34,7 +34,7 @@ describe('getCategory', () => {
   })
 });
 
-describe('getSubategory', () => {
+describe('getSubcategory', () => {
   it('should return subcategories', async () => {
     const mockResolvedValue = [
       { id: 1, name: 'Horror' },
@@ -64,4 +64,36 @@ describe('getSubategory', () => {
     await expect(getSubcategory('1')).rejects.toThrow('PostGreSQL Error');
   })
 });
+
+describe('getQuiz', () => {
+  it('should return quizzes', async () => {
+    const mockResolvedValue = [
+      { id: 1, name: 'Horror Quiz' },
+      { id: 2, name: 'Star Wars Quiz' },
+      { id: 3, name: 'Lord of the Rings Quiz' },
+    ];
+
+    const queryDatabaseMock = jest.spyOn(db, 'queryDatabase');
+    queryDatabaseMock.mockResolvedValue(mockResolvedValue);
+    
+    const result = await getQuiz('1', '3');
+
+    expect(result).toEqual([
+      { id: 1, name: 'Horror Quiz' },
+      { id: 2, name: 'Star Wars Quiz' },
+      { id: 3, name: 'Lord of the Rings Quiz' },
+    ]);
+
+    queryDatabaseMock.mockRestore();
+  });
+
+  it('should handle an error', async () => {
+    const queryDatabaseMock = jest.spyOn(db, 'queryDatabase');
+
+    queryDatabaseMock.mockRejectedValue(new Error('PostGreSQL Error'))
+
+    await expect(getQuiz('1', '3')).rejects.toThrow('PostGreSQL Error');
+  })
+});
+
 
