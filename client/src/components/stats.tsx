9bc758ba69e51fc84d61b319bd63a58491a77aa7
Sjlowman <sarah.j.lowman@gmail.com>
2023-08-09 13:41:30 +0100

    Tailwind updates for stats table

--- client/src/components/stats.tsx
+++ client/src/components/stats.tsx
@@ -11,7 +11,7 @@ interface Stats {
   answered_today: number;
   answered_total: number;
   quiz: string;
-  topic: string
+  topic: string;
 }
 
 const Stats: React.FC = () => {
@@ -36,42 +36,50 @@ const Stats: React.FC = () => {
     fetchRound();
   }, [roundId]);
 
-  return stats.length? (
-    <section>
+  return stats.length ? (
+    <section className="w-full bg-light">
       <table>
-          <tbody>
-            <tr>
-              <th>Quizzes Played Today:</th>
-              <th>Quizzes Played Total:</th>
-            </tr>
-            <tr>
-              <td>{stats[0].played_today}</td>
-              <td>{stats[0].played_total}</td>
-            </tr>
-            <tr>
-              <th>Average Score Today:</th>
-              <th>Average Score Today:</th>
-            </tr>
-            <tr>
-              <td>{Math.round(stats[0].correct_today * 100 / stats[0].answered_today)}%</td>
-              <td>{Math.round(stats[0].correct_total * 100 / stats[0].answered_total)}%</td>
-            </tr>
-            <tr>
-              <th>Favourite Quiz:</th>
-              <th>Favourite Topic:</th>
-            </tr>
-            <tr>
-              <td>{stats[0].quiz}</td>
-              <td>{stats[0].topic}</td>
-            </tr>
-          </tbody>
-        </table>
+        <tbody>
+          <tr className=" text-darkest font-bold text-center">
+            <th>Quizzes Played Today:</th>
+            <th>Quizzes Played Total:</th>
+          </tr>
+          <tr className=" text-darkest text-center">
+            <td>{stats[0].played_today}</td>
+            <td>{stats[0].played_total}</td>
+          </tr>
+          <tr className=" text-darkest font-bold text-center">
+            <th>Average Score Today:</th>
+            <th>Average Score Today:</th>
+          </tr>
+          <tr className=" text-darkest text-center">
+            <td>
+              {Math.round(
+                (stats[0].correct_today * 100) / stats[0].answered_today
+              )}
+              %
+            </td>
+            <td>
+              {Math.round(
+                (stats[0].correct_total * 100) / stats[0].answered_total
+              )}
+              %
+            </td>
+          </tr>
+          <tr className=" text-darkest font-bold text-center">
+            <th>Favourite Quiz:</th>
+            <th>Favourite Topic:</th>
+          </tr>
+          <tr className=" text-darkest text-center">
+            <td>{stats[0].quiz}</td>
+            <td>{stats[0].topic}</td>
+          </tr>
+        </tbody>
+      </table>
     </section>
   ) : (
     <section>
-      <p>
-        Loading Stats...
-      </p>
+      <p>Loading Stats...</p>
     </section>
   );
 };
