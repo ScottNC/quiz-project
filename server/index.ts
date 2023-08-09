a7b9831d257c489d7657559ab047457c9b8011c2
ScottNC <133363623+ScottNC@users.noreply.github.com>
2023-08-09 20:33:30 +0100

    adds monthly schedule to repopulate the database

--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,8 @@
 import express, { Express } from 'express';
 import cors from 'cors';
+import cron from 'node-cron';
 import { setupRoutes } from './routes/routes';
+import { populateQuestions, populateQuiz } from './utils/database_populator';
 
 const app: Express = express();
 const port: number = 3000;
@@ -11,4 +13,11 @@ setupRoutes(app);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
+});
+
+
+// run these functions on the first day of each month
+cron.schedule('0 0 1 * *', async () => {
+  await populateQuestions();
+  await populateQuiz();
 });
