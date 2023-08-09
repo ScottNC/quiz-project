9647879494b9fce04f1bd799252e72d0d70dfbd4
ScottNC <133363623+ScottNC@users.noreply.github.com>
2023-08-09 19:17:13 +0100

    stats page looks nicer

--- client/src/components/stats.tsx
+++ client/src/components/stats.tsx
@@ -27,45 +27,50 @@ const StatsPage: React.FC = () => {
   }, [roundId]);
 
   return stats.length ? (
-    <section className="w-full bg-light">
-      <table>
-        <tbody>
-          <tr className=" text-darkest font-bold text-center">
-            <th>Quizzes Played Today:</th>
-            <th>Quizzes Played Total:</th>
-          </tr>
-          <tr className=" text-darkest text-center">
-            <td>{stats[0].played_today}</td>
-            <td>{stats[0].played_total}</td>
-          </tr>
-          <tr className=" text-darkest font-bold text-center">
-            <th>Average Score Today:</th>
-            <th>Average Score Today:</th>
-          </tr>
-          <tr className=" text-darkest text-center">
-            <td>
-              {Math.round(
-                (stats[0].correct_today * 100) / stats[0].answered_today
-              )}
-              %
-            </td>
-            <td>
-              {Math.round(
-                (stats[0].correct_total * 100) / stats[0].answered_total
-              )}
-              %
-            </td>
-          </tr>
-          <tr className=" text-darkest font-bold text-center">
-            <th>Favourite Quiz:</th>
-            <th>Favourite Topic:</th>
-          </tr>
-          <tr className=" text-darkest text-center">
-            <td>{stats[0].quiz}</td>
-            <td>{stats[0].topic}</td>
-          </tr>
-        </tbody>
-      </table>
+    <section className="w-full h-screen bg-light">
+      <div className="text-2xl p-8 text-dark font-bold text-center">
+        Monthly Stats
+      </div>
+      <div className="grid place-items-center p-12">
+        <table>
+          <tbody>
+            <tr className=" text-darkest font-bold text-center p-12">
+              <th>Quizzes Played Today:</th>
+              <th>Quizzes Played This Month:</th>
+            </tr>
+            <tr className=" text-darkest text-center p-4">
+              <td>{stats[0].played_today}</td>
+              <td>{stats[0].played_total}</td>
+            </tr>
+            <tr className=" text-darkest font-bold text-center  p-12">
+              <th>Average Score Today:</th>
+              <th>Average Score This Month:</th>
+            </tr>
+            <tr className=" text-darkest text-center p-4">
+              <td>
+                {Math.round(
+                  (stats[0].correct_today * 100) / stats[0].answered_today
+                )}
+                %
+              </td>
+              <td>
+                {Math.round(
+                  (stats[0].correct_total * 100) / stats[0].answered_total
+                )}
+                %
+              </td>
+            </tr>
+            <tr className=" text-darkest font-bold text-center  p-12">
+              <th>Favourite Quiz:</th>
+              <th>Favourite Topic:</th>
+            </tr>
+            <tr className=" text-darkest text-center  p-4">
+              <td>{stats[0].quiz}</td>
+              <td>{stats[0].topic}</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </section>
   ) : (
     <section>
