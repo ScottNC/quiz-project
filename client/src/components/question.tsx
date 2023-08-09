d125caf85861daaac7de05a64d8659a73ceb2d5b
ScottNC <133363623+ScottNC@users.noreply.github.com>
2023-08-09 20:26:32 +0100

    height of button changes if answer text is too large to fit

--- client/src/components/question.tsx
+++ client/src/components/question.tsx
@@ -117,7 +117,7 @@ const Questions: React.FC = () => {
 
     return (
       backgroundColour +
-      " p-3 w-40 h-16 text-light font-bold rounded-lg cursor-pointer select-none active:translate-y-2 active:[box-shadow:0_0px_0_0_#40798C,0_0px_0_0_#1b70f841] active:border-b-[0px] transition-all duration-150 [box-shadow:0_10px_0_0_#40798C,0_15px_0_0_#1b70f841] border-b-[1px] border-blue-400"
+      " p-3 w-40 min-h-16 text-light font-bold rounded-lg cursor-pointer select-none active:translate-y-2 active:[box-shadow:0_0px_0_0_#40798C,0_0px_0_0_#1b70f841] active:border-b-[0px] transition-all duration-150 [box-shadow:0_10px_0_0_#40798C,0_15px_0_0_#1b70f841] border-b-[1px] border-blue-400"
     );
   };
 
